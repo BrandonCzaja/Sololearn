16a8d760018fba5d1bed72ce0d481735c06ba86f
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-03-05 18:32:18 -0600

    Completed Basic Concepts

---
+++ Python/Basic_Concepts/Welcome_to_Python.py
@@ -0,0 +1,25 @@
+print('Hello world!')
+print('Hi everybody!')
+print((4+8)/2)  # Produces a float
+
+# Exponentiation
+print(2**5)
+print(4**8)
+
+
+# To find the root of a number
+print(9**(1/2))
+print(8**(1/3))
+
+# Quotient: Floor division is done using two forward slashes and is used to determine the quotient of a division
+print(20 // 6)  # Return the quotient rounded down
+print(20/6)
+
+
+# Module - Same as JS
+print(20 % 6)
+print(7 % (5//2))
+
+
+# Examples
+print((3**2//2))
