b3746c582fe389db14a46adb741683d9faa9fd16
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-03-05 23:12:58 -0600

    Multiline quotes

--- Python/Strings/Strings_&_Variables.py
+++ Python/Strings/Strings_&_Variables.py
@@ -8,3 +8,7 @@ print("And then he said \"Hi everybody!\" and we all laughed")
 
 # Newlines: \n
 print('Line One \nLine Two, \nLine Three')
+
+print("""this is
+a multiline 
+text""")  # Three quotes automatically creates a multiline quote where you indicate it to
