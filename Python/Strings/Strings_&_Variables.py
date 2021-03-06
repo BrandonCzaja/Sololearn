e47e31907a4d44447e7a95eea7a8295e8613355c
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-03-05 23:10:39 -0600

    Practiced escaping characters and new lines

--- Python/Strings/Strings_&_Variables.py
+++ Python/Strings/Strings_&_Variables.py
@@ -1,2 +1,10 @@
 print('Python is fun!')
 print('Always look on the bright side')
+
+
+# Escaping characters
+print("And then he said \"Hi everybody!\" and we all laughed")
+
+
+# Newlines: \n
+print('Line One \nLine Two, \nLine Three')
