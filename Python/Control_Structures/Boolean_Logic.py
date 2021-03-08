b0907a9734ae04616203ec47caa0c89860bd0f93
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-03-08 11:45:22 -0600

    Practiced with the OR operator and the NOT operator

--- Python/Control_Structures/Boolean_Logic.py
+++ Python/Control_Structures/Boolean_Logic.py
@@ -1,4 +1,6 @@
 # Boolean logic operators are (and, or, not)
+
+# And Operator
 # print(1 == 1 and 2 == 2)
 # print(1 == 1 and 2 == 3)
 # print(1 != 1 and 2 == 2)
@@ -11,3 +13,22 @@ if (1 == 1 and 2 + 2 > 3):
     print('true')
 else:
     print('false')
+
+
+# Or Operator
+age = 15
+money = 500
+if age > 18 or money > 100:
+    print('Welcome')
+
+
+# Not Operator
+# print(not 1 == 1)  # False
+# print(not 1 > 7)  # True
+
+if not True:
+    print('1')
+elif not (1 + 1 == 3):
+    print("2")
+else:
+    print("3")
