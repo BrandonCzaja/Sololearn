84d61f7ad3cc1588d8eb1b4be6e9f7c506162b39
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-03-08 11:41:39 -0600

    Used an if else statement to compare logical expressions then wrote myself a note about syntax

---
+++ Python/Control_Structures/Boolean_Logic.py
@@ -0,0 +1,13 @@
+# Boolean logic operators are (and, or, not)
+# print(1 == 1 and 2 == 2)
+# print(1 == 1 and 2 == 3)
+# print(1 != 1 and 2 == 2)
+# print(2 < 1 and 3 > 6)
+
+
+# Example of boolean logic with an if statement
+# I can either leave the expressions unwrapped, wrap each individual statement or wrap the whole if condition in ()
+if (1 == 1 and 2 + 2 > 3):
+    print('true')
+else:
+    print('false')
