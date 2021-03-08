1087fcc993c05b97945c55ae717fcd61c2c1f4c3
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-03-08 12:00:20 -0600

    Finished Boolean Logic. Practiced making and nesting Lists

---
+++ Python/Control_Structures/Lists.py
@@ -0,0 +1,17 @@
+# Lists are the python equivalent to js arrays
+
+nums = [5, 4, 3, 2, 1]
+print(nums[1])
+
+# Empty List
+empty_list = []
+print(empty_list)
+
+# Storing multiple data types in a single list
+number = 3
+things = ['string', 0, [1, 2, number], 4.56]
+
+
+# Nesting Lists
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(matrix[1][2])  # prints 6
