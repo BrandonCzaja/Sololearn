41c3df292c337a5fdd44b2f41a2018059e6146d5
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-03-09 21:54:46 -0600

    Practiced with the In and Not Operators

--- Python/Control_Structures/Lists.py
+++ Python/Control_Structures/Lists.py
@@ -21,3 +21,32 @@ print(matrix[1][2])  # prints 6
 str = "Hello world!"
 print(str[5])  # Prints the blank space
 print(str[6])
+
+
+# List Operations
+nums = [7, 7, 7, 7, 7, 7, 7]
+nums[2] = 5
+print(nums)
+print(nums + [4, 5, 6])
+# Increases the list size by 3x but does not increase the indexes' values
+print(nums * 3)
+
+
+# In Operator
+# Checks if an item is in the list
+words = ["spam", 'eggs', 'green', 'sausage']
+print("spam" in words)  # Prints True
+print('eggs' in words)  # Prints True
+print('tomato' in words)  # Prints False
+
+# Not Operator is the opposite of the In Operator
+nums = [1, 2, 3]
+print(not 4 in nums)
+print(4 not in nums)
+print(not 3 in nums)
+print(3 not in nums)
+
+if "spam" in words:
+    print('Yes')
+    if 2 in nums:
+        print("Also Yes")
