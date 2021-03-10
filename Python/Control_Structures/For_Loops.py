ee6c13421281c461bc3d2e1ded1ffb63716c4832
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-03-09 22:51:35 -0600

    Finished For Loops. For some reason my Continue Statements still print the iteration they should be skipping. I will have to look into the syntax to make sure I am doing it right

---
+++ Python/Control_Structures/For_Loops.py
@@ -0,0 +1,23 @@
+words = ['hello', 'greetings', 'welcome']
+for cheese in words:
+    print(cheese + "!")
+
+
+str = 'practicing for loops'
+count = 0
+iteration = 0
+for char in str:
+    print(iteration)
+    iteration += 1
+    if iteration == 5:
+        continue
+        print('Continue')
+
+    if iteration > 10:
+        break
+
+    if char == 't':
+        count += 1
+
+print(count)
+print(iteration)
