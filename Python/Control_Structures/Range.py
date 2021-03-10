3f14380b66c7c39c6b3d7b0b02cb84b39b542b23
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-03-09 23:00:26 -0600

    Practiced making ranges then looping through them

---
+++ Python/Control_Structures/Range.py
@@ -0,0 +1,13 @@
+# The range function returns a sequence of numbers. By default it starts from 0 and increments by 1 and stops before the specified number
+numbers = (list(range(10)))
+# print(numbers)
+
+more_numbers = list(range(20, 40))
+# print(more_numbers)
+
+my_object = range(20)
+print(my_object)
+
+for num in my_object:
+    print(num)
+# print(range(20) == range(0, 20))
