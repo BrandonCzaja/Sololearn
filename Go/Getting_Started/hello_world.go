04b93b5781ec7824d3aa4e14d955048117959d37
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-07-14 15:16:12 -0500

    Finished Module 1: Getting Started

---
+++ Go/Getting_Started/hello_world.go
@@ -0,0 +1,6 @@
+package main // declares that this is the main package of the program
+import "fmt" // an imported package. Provides input/output functionality
+
+func main() {
+	fmt.Println("Hello, World!")
+}
